b4bce25da0c94f49f325589e9a85fc48db26cb11
Pawel <bragoszewski@gmail.com>
2026-02-15 17:05:57 +0100

    more robust deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 git pull --rebase
+docker-compose down
+docker rm -f steam-monitor 2>/dev/null || true
 docker-compose up -d
 docker logs --tail 50 steam-monitor
